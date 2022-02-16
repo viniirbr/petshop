6e48926a2000a9bc231639da48a73adc50ad1ca5
Vinicius Ribeiro <vayne99@gmail.com>
2022-02-16 11:22:03 -0300

    Installing files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <form>
+      <label>Cliente</label>
+      <input placeholder='Nome do cliente'></input>
+      
+    </form>
+  );
+}
+
+export default App;
