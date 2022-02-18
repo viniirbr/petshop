f43b36bf4038884aa32308763259285a490d8f69
Vinicius Ribeiro <vayne99@gmail.com>
2022-02-18 11:36:49 -0300

    Dealing with states in the form

--- src/components/ScheduleForm/ScheduleForm.jsx
+++ src/components/ScheduleForm/ScheduleForm.jsx
@@ -1,36 +1,101 @@
 import React, { Component } from 'react';
 import './schedule-form.css'
+import moment from 'moment'
+
 
 class ScheduleForm extends Component {
+
+    constructor() {
+        super();
+        this.petName = "";
+        this.humanName = "";
+        this.sevice = "Shower";
+        this.scheduleDate = new moment().format("YYYY-MM-DD");
+    }
+
+    changePetName(event) {
+        this.petName = event.target.value;
+        this.setState({
+            petName: this.petName
+        })
+    }
+
+    changeHumanName(event) {
+        this.humanName = event.target.value;
+        this.setState({
+            humanName: this.humanName
+        })
+    }
+
+    changeService(event) {
+        this.service = event.target.value;
+        console.log(event.target.value);
+        this.setState({
+            service: this.service
+        })
+    }
+
+    changeDate(event) {
+        this.scheduleDate = event.target.value;
+        this.setState({
+            scheduleDate: this.scheduleDate
+        })
+    }
+
+    alertClick(event) {
+        event.preventDefault()
+        const info = {
+            petName: this.petName,
+            humanName: this.humanName,
+            service: this.service,
+            shceduleDate: this.scheduleDate
+        }
+        console.log(info);
+        this.petName = "";
+        this.humanName = "";
+        this.service = "";
+        this.scheduleDate = new moment().format("YYYY-MM-DD");
+        this.setState(info)
+    }
 
     render() {
         return (
             <form className='schedule-form'>
                 <div className='schedule-form__container'>
                     <label className='schedule-form__label'>Pet name</label>
-                    <input type='text' placeholder='Pet name...' className='input' />
+                    <input
+                        type='text'
+                        placeholder='Pet name...'
+                        className='input'
+                        value={this.petName}
+                        onChange={this.changePetName.bind(this)} />
                 </div>
 
                 <div className='schedule-form__container'>
                     <label className='schedule-form__label'>His/Her human</label>
-                    <input type='text' placeholder='Human responsible for the pet...' className='input' />
+                    <input 
+                    type='text' 
+                    placeholder='Human responsible for the pet...' 
+                    className='input'
+                    value={this.humanName}
+                    onChange={this.changeHumanName.bind(this)} />
                 </div>
 
                 <div className='schedule-form__container'>
                     <label className='schedule-form__label'>Select service</label>
-                    <select>
-                        <option>Shower</option>
-                        <option>Groom</option>
-                        <option>Showe+Groom</option>
+                    <select value={this.service} onChange={this.changeService.bind(this)}>
+                        <option value="Shower">Shower</option>
+                        <option value="Groom">Groom</option>
+                        <option value="Shower+Groom">Shower+Groom</option>
                     </select>
                 </div>
 
                 <div className='schedule-form__container'>
                     <label className='schedule-form__label'>Choose a date</label>
-                    <input type="date"/>
+                    <input type="date" value={this.scheduleDate} onChange={this.changeDate.bind(this)}/>
                 </div>
 
-                <button className='button'>Confirm</button>
+                <button className='button' onClick={this.alertClick.bind(this)}>Confirm</button>
             </form>
         )
     }
